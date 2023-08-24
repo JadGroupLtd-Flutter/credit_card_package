ef474895f85d84fe6afe350f937b973c6884b34a
Mohamed Adel Hosny <mohamed.adel@premiumcard.net>
2023-08-24 11:02:54 +0300

    first commit

---
+++ lib/credit_card_brand.dart
@@ -0,0 +1,7 @@
+import 'package:credit_card_package/credit_card_widget.dart';
+
+class CreditCardBrand {
+  CreditCardBrand(this.brandName);
+
+  CardType? brandName;
+}
---
+++ lib/custom_card_type_icon.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:credit_card_package/credit_card_widget.dart';
+
+class CustomCardTypeIcon {
+  /// A model class to update card image with user defined widget for the
+  /// [CardType].
+  CustomCardTypeIcon({
+    required this.cardType,
+    required this.cardImage,
+  });
+
+  /// Specify type of the card available in the parameter of enum.
+  CardType cardType;
+
+  /// Showcasing widget for specified card type.
+  Widget cardImage;
+}
